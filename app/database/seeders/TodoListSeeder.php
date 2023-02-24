14ab1ce7e41acebf38b1548714129f11862a2e87
masahiroyamasaki <y.masahiro123@gmail.com>
2023-02-25 03:58:30 +0900

    Initial commit

---
+++ app/database/seeders/TodoListSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TodoListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todo_lists')->insert(
+            [
+                [
+                'name' => 'テスト1',
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],
+                [
+                'name' => 'テスト2',
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],
+                [
+                'name' => 'テスト3',
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],
+            ]
+          ); //
+    }
+}
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'sky': '#87ceeb',
+      }
+    },
   },
   plugins: [],
-}
+};
